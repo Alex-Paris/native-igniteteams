6419b19d6b72d044b87332a3b15adb3a24038680
Alex Paris <alex_paris28@hotmail.com>
2023-08-30 11:16:13 -0300

    chore: empty list and button components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
+
 import { Header } from '@components/Header';
-import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 
+import { Container } from './styles';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
 export function Groups() {
+  const [groups, setGroups] = useState([])
+  
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='jogue com a sua turma' />
       
-      <GroupCard title='Dota' />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        renderItem={({item}) => (<GroupCard title={item} />)}
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
+      />
+
+      <Button title='Criar nova turma' />
     </Container>
   );
 }
