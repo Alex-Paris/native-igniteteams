97dc8b6431acf29bfa95ac92db87d8e0bee41b61
Alex Paris <alex_paris28@hotmail.com>
2023-08-31 17:44:21 -0300

    chore: player card

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 
 import { Header } from '@components/Header';
@@ -6,15 +7,16 @@ import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
-import { useState } from 'react';
 
 interface PlayersProps {}
 
 export function Players({  }: PlayersProps) {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState(['Sean Anthony', 'Gaveta'])
 
   return (
     <Container>
@@ -49,6 +51,21 @@ export function Players({  }: PlayersProps) {
         </NumberOfPlayers>
       </HeaderList>
       
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => <ListEmpty message='Não há pessoas nesse time' />}
+      />
+
+      <Button title='Remover Turma' type='SECONDARY' />
     </Container>
   );
 }
